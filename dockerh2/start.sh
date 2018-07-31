f518928265e288c2e25b5fe96c12d268c0ae8429
schikkam <shiva.chikkam@gmail.com>
2018-07-31 17:10:30 +0530

    No local dependecies

--- dockerh2/start.sh
+++ dockerh2/start.sh
@@ -3,7 +3,7 @@ echo "\nStarting H2 As A Service @ Startup Containers\n"
 cd /softwares/h2/bin
 nohup java -cp h2*.jar org.h2.tools.Server -webAllowOthers & 
 echo "waiting to bringup H2 Process"
-sleep 10
+sleep 180
 nc -v localhost 8082
 if [ $? -eq 0 ] 
 then
@@ -11,5 +11,3 @@ then
 else
   echo "\nFailed In Starting Up H2"
 fi
-exit
-
